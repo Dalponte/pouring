b679e8da2688aeb76e6da8dba4e4ced49580fe91
Maico Dal Ponte <dalpontemaico@gmail.com>
2025-03-20 16:35:25 -0300

    feat: reset database

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MqttModule } from './mqtt/mqtt.module';
 import { OperationProcessor } from './queue/processors/operation.processor';
 import { QueueModule } from './queue/queue.modules';
 import { PrismaModule } from './prisma/prisma.module';
+import { join } from 'path';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        }),
         MqttModule,
         QueueModule,
         PrismaModule,
