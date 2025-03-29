7d9625ed3fd3d4072efd1e307878fa0e0a47c410
Maico Dal Ponte <dalpontemaico@gmail.com>
2025-03-29 16:21:16 -0300

    feat: add dashboard project

---
+++ apps/dashboard/src/App.tsx
@@ -0,0 +1,14 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { ThemeToggle } from "./components/theme-toggle"
+
+function App() {
+  return (
+    <ThemeToggle />
+  )
+}
+
+export default App
