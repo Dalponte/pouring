f6b00c96aff54a92b8d2ba20a3806f091ad93a35
Maico Dal Ponte <dalpontemaico@gmail.com>
2025-03-30 17:00:50 -0300

    feat: remove subscription for now

--- apps/dashboard/src/hooks/useTaps.ts
+++ apps/dashboard/src/hooks/useTaps.ts
@@ -1,50 +1,12 @@
 import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import { GET_TAPS, GET_TAP } from "@/lib/graphql/queries";
 import { CREATE_TAP, UPDATE_TAP, SOFT_DELETE_TAP, RESTORE_TAP, HARD_DELETE_TAP } from "@/lib/graphql/mutations";
-import { TAP_ADDED, TAP_UPDATED } from "@/lib/graphql/subscriptions";
 import { Tap } from "@/lib/types/graphql";
 
 export function useTaps(includeDeleted = false) {
     const { data, loading, error, refetch } = useQuery(GET_TAPS, {
         variables: { includeDeleted },
         fetchPolicy: "cache-and-network",
-    });
-
-    // Subscribe to new taps being added
-    useSubscription(TAP_ADDED, {
-        onData: ({ client, data }) => {
-            if (data.data?.tapAdded) {
-                client.cache.updateQuery(
-                    { query: GET_TAPS, variables: { includeDeleted } },
-                    (existingData) => {
-                        if (!existingData) return { getTaps: [data.data.tapAdded] };
-                        return {
-                            getTaps: [...existingData.getTaps, data.data.tapAdded],
-                        };
-                    }
-                );
-            }
-        },
-    });
-
-    // Subscribe to tap updates
-    useSubscription(TAP_UPDATED, {
-        onData: ({ client, data }) => {
-            if (data.data?.tapUpdated) {
-                const updatedTap = data.data.tapUpdated;
-                client.cache.updateQuery(
-                    { query: GET_TAPS, variables: { includeDeleted } },
-                    (existingData) => {
-                        if (!existingData) return { getTaps: [updatedTap] };
-                        return {
-                            getTaps: existingData.getTaps.map((tap: Tap) =>
-                                tap.id === updatedTap.id ? updatedTap : tap
-                            ),
-                        };
-                    }
-                );
-            }
-        },
     });
 
     const [createTapMutation] = useMutation(CREATE_TAP, {
